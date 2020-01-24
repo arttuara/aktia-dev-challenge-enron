079459fabdd542d712c7913cd266830bd6702897
Arttu Arponen <arttu.arponen@aalto.fi>
2020-01-24 16:40:04 +0200

    added pathfinding

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import csv
 
 # Function for finding all paths for different users.
 def find_paths(path):
-    paths = []
+    paths = []  # Saving all paths here.
+
     try:
         # Loop through directory and save different paths to 'paths' variable.
         for folder in os.listdir(path):
@@ -16,13 +17,41 @@ def find_paths(path):
     except IOError as e:
         print("An error ocurred in path: {}".format(path))
         print(e)
+
     return paths    # Return list of paths
+
+
+def fetch_recipients(path):
+    sender = []     # Saving sender here.
+    dataset = {}    # Saving recipients here with the number of mails. Key:Value = 'recipient':'count'.
+
+    try:
+        # Listing subfolders in users folder.
+        for subfolders in os.listdir(path):
+            # Search folders 'sent' or 'sent_items' and pass them forward.
+            if subfolders == "sent" or subfolders == "sent_items":
+                path1 = path + "/" + subfolders
+                try:
+                    # Go through files in the folders and filter data.
+                    for files in os.listdir(path1):
+                        path2 = path1 + "/" + files
+                        print(path2) #test print
+                except IOError as e:
+                    print("An error ocurred in path: {}".format(path1))
+                    print(e)
+    except IOError as e:
+        print("An error ocurred in path: {}".format(path))
+        print(e)
+
+    return dataset, sender  # Return the dataset and sender
+
+
 
 def main():
     # Check if '/maildir' path exists.
     path = os.path.dirname(os.path.realpath(__file__)) + "/maildir"
     if os.path.exists(path):    # Path OK.
-        print("Path OK. Starting data management.")
+        print("Path OK.\nStarting data management.")
 
         # Initialize csv file.
         try:
@@ -36,7 +65,8 @@ def main():
 
         # Finging different user paths.
         paths = find_paths(path)
-        print(paths) #test print
+        # Fetching recipient data
+        dataset, sender = fetch_recipients(paths[0])
 
         sys.exit()
 
